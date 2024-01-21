5ebe6d552489ebe340f7ea8b7d08f92de2a69e27
MostafaAlaa297 <elaskary609@gmail.com>
2024-01-21 09:42:31 -0800

    edit selection sort logic

--- 2-main.c
+++ 2-main.c
@@ -9,7 +9,9 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	int array[] = {45, -12, 27, -5, 19, -8, 33, -2, 11, -6};
+
+    /*int array[] = {19, 48, -99, 71, 13, 52, 96, 73, 86, 7};*/
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,22 +15,24 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; ++i)
 	{
-		int key, temp;
+		int temp;
 
-		key = array[i];
 		min_idx = i;
 
 		for (j = i + 1; j < size; ++j)
 		{
-			if (key > array[j])
+			if (array[j] < array[i])
 			{
 				min_idx = j;
 			}
 		}
-
-		temp = array[min_idx];
-		array[min_idx] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		
+		if (min_idx != i)
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
